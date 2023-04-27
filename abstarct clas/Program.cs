a34a6f88cb636b703fc1a0cb6df00d751ff3c999
aanchal-subedi <aanchalsubedi005@gmail.com>
2023-04-27 12:37:42 +0545

    first commit

---
+++ abstarct clas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+abstract class AreaClass
+{
+    abstract public int Area();
+}
+class Square : AreaClass
+{
+    int side = 0;
+    public Square (int n)
+    {
+        side = n;
+    }
+    public override int Area()
+    {
+        return side * side;
+    }
+}
+class Driver
+{
+    public static void Main()
+    {
+        Square s = new Square(6);
+        Console.WriteLine("Area =" + s.Area());
+    }
+}
